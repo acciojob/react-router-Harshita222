5a09a2580e2c3df070cbf91dc3068f1ff0c75530
Harshita222 <kumariharsita501@gmail.com>
2024-07-01 18:45:14 +0530

    do

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div>
         <h2>About</h2>
-      <p>This is a sample React Router Program</p>
+      <p>This is a sample React Router program.</p>
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,20 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
-import Switch from "./Switch";
+import Layout from "./Layout";
 
 
 const App= ()=>{
-
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route path="" element={<Switch />} >
-                    <Route path="" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="about" element={<About />} />
+            </Route>
+        </Routes>
+    </BrowserRouter>
+);
 }
 
 export default App
